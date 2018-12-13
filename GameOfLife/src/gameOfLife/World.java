2b9ac6eec81bbccc254cad670a5f4a5a1d60ba3b
s-kimbr <s-kimbr@51S500036857.it.bsd405.org>
2018-12-13 12:55:41 -0800

    Checkpoint 1

--- GameOfLife/src/gameOfLife/World.java
+++ GameOfLife/src/gameOfLife/World.java
@@ -11,16 +11,16 @@ public class World implements TileGrid
 		this.columns=columns;
 		core.API.initialize(rows, columns);
 		x= new Tile[rows][columns];
-		for(int z=1;z<=rows;z++)
+		for(int z=0;z<rows;z++)
 		{
-			for(int y=1; y<=columns;y++)
+			for(int y=0; y<columns;y++)
 			{
 				x[z][y]=new ConstantTile(0);
 			}
 		}
-		for(int z=1;z<=rows;z++)
+		for(int z=0;z<rows;z++)
 		{
-			for(int y=1; y<=columns;y++)
+			for(int y=0; y<columns;y++)
 			{
 				core.API.paintSolidColor(rows, columns,x[z][y].getColor());
 			}
