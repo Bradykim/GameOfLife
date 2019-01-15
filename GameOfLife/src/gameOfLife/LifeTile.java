bd65affee4a2dda03f6bf8aea093f9849e087480
s-kimbr <s-kimbr@51S50023421.it.bsd405.org>
2019-01-15 14:39:53 -0800

    djwadwauwaud

---
+++ GameOfLife/src/gameOfLife/LifeTile.java
@@ -0,0 +1,35 @@
+package gameOfLife;
+
+import java.awt.Color;
+
+public abstract class LifeTile
+{
+	private int ageTile;
+	public LifeTile(int ageTile)
+	{
+		this.ageTile=ageTile;
+	}
+	public int getAge()
+	{
+		return this.ageTile;
+	}
+	
+	public abstract Color getColor();
+	
+	public abstract LifeTile getUpdatedtile(Tile [] neighbors);
+	
+	 public int getNumActiveNeighbors (Tile[] neighbors)
+	 {
+		 int active =0;
+		 int spaces= neighbors.length;
+		 for(int x=0; x<spaces;x++)
+		 {
+			 if(neighbors[x].getAge()>0)
+			 {
+				 active++;
+			 }
+		 }
+		 return active;
+		 
+	 }
+}
---
+++ GameOfLife/src/gameOfLife/MonoTile.java
@@ -0,0 +1,48 @@
+package gameOfLife;
+
+import java.awt.Color;
+
+public class MonoTile extends LifeTile
+{
+	public MonoTile(int initialAge)
+	{
+		super(initialAge);
+	}
+	public Color getColor()
+	{
+		Color r = null;
+		if(getAge()==0)
+		{
+			r= Color.BLACK;
+		}
+		else if(getAge()==1)
+		{
+			r= Color.WHITE;
+		}
+		return r;
+	}
+	public MonoTile getUpdatedtile(Tile[] neighbors)
+	{
+		int active= getNumActiveNeighbors(neighbors);
+		if(getAge()>0)
+		{
+			if(active<2 || active>3)
+			{
+				return new MonoTile(0);
+			}
+			else
+			{
+				return new MonoTile(getAge()+1);
+			}
+		}
+		if(getAge()==0)
+		{
+			if(active==3)
+			{
+				return new MonoTile (1);
+			}
+			
+		}
+		return new MonoTile(0);
+	}
+}
