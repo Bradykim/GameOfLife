0c548ac564b3a249fa03fc4b6898ce7f39643fbf
s-kimbr <s-kimbr@51S50023421.it.bsd405.org>
2019-01-17 15:05:24 -0800

    sadadwwa

--- GameOfLife/src/gameOfLife/World.java
+++ GameOfLife/src/gameOfLife/World.java
@@ -164,7 +164,7 @@ public class World implements TileGrid
 					//code for tile to the bottom left
 					z[7]= y[LeftOne][DownOne];
 					
-					Tile hi = y[i][j].getUpdatedTile(z);
+					Tile hi = x[i][j].getUpdatedTile(z);
 					y[i][j] = hi;
 				}
 			}
