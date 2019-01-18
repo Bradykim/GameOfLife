9cf6241d941ca27c860fa3c889e12d4c975b390e
s-kimbr <s-kimbr@51S50023421.it.bsd405.org>
2019-01-17 18:38:23 -0800

    dwadwa

--- GameOfLife/src/gameOfLife/World.java
+++ GameOfLife/src/gameOfLife/World.java
@@ -108,82 +108,80 @@ public class World implements TileGrid
 		}
 		if (f.equals("evolve"))
 		{
-			// Loop -> all the tiles in a new grid
 			
-			// In loop
-				// Call get ns
-				// Put the value in to get update tilke
-				// Put that tile in to the place of the array
-			
-			//Outside of loop
-			//Draw
 			String pause= s[2];
 			int pauseMs= Integer.parseInt(pause);
 			String steps1= s[1];
 			int steps= Integer.parseInt(steps1);
 			Tile[][] y = new Tile [rows][columns];
-			for(int r=0;r<rows;r++)
+			for(int i=0; i<steps;i++)
 			{
-				for(int c=0; c<columns;c++)
+				for(int r=0;r<rows;r++)
 				{
-					Tile[] z = new Tile[8];
-					
-					int UpOne= (c+1);
-					if((c+1)>(columns-1))
+					for(int c=0; c<columns;c++)
 					{
-						UpOne= (c+1)%columns;
-					}
-					int DownOne= (c-1);
-					if((c-1)<0)
-					{
-						DownOne= ((c-1)+columns)%columns;
-					}
-					int RightOne= (r+1);
-					if((r+1)>(columns-1))
-					{
-						RightOne=(r+1)%rows;
-					}
-					
-					int LeftOne= (r-1)%rows;
-					if((r-1)<0)
-					{
-						LeftOne= ((r-1)+rows)%rows;
-					}
-					
-					//Code for Tile right above
-					z[0]= y[r][UpOne];
-					
-					//Code for tile to the Right
-					z[1]= y[RightOne][c];
-					
-					//Code for Tile right below
-					z[2]= y[r][DownOne];
-					
-					//Code for Tile to the left
-					z[3]= y[LeftOne][c];
-					
-					//code for Tile to the Upper left
-					z[4]= y[LeftOne][UpOne];
-					
-					//Code for tile to the upper right
-					z[5]= y[RightOne][UpOne];
-					
-					//code for tile to the bottom right
-					z[6]= y[RightOne][DownOne];
-					
-					//code for tile to the bottom left
-					z[7]= y[LeftOne][DownOne];
-					
-						for(int o=0; o<z.length;o++)
-						{
-							System.out.println(z[o]);
-						}
-						System.out.println("/n");
+						Tile[] z = new Tile[8];
 						
-					Tile hi = x[r][c].getUpdatedTile(z);
-					y[r][c] = hi;
+						int UpOne= (r-1);
+						if((r-1)<0)
+						{
+							UpOne= (((r-1)+rows)%rows);
+						}
+						
+						int DownOne= (r+1);
+						if((r+1)>rows)
+						{
+							DownOne= (r+1)%rows;
+						}
+						
+						int RightOne= (c+1);
+						if((c+1)>columns)
+						{
+							RightOne=(c+1)%columns;
+						}
+						
+						int LeftOne= (c-1);
+						if((c-1)<0)
+						{
+							LeftOne= ((c-1)+columns);
+						}
+						
+						//Code for Tile right above
+						z[0]= y[UpOne][c];
+						
+						//Code for tile to the Right
+						z[1]= y[r][RightOne];
+						
+						//Code for Tile right below
+						z[2]= y[DownOne][c];
+						
+						//Code for Tile to the left
+						z[3]= y[r][LeftOne];
+						
+						//code for Tile to the Upper left
+						z[4]= y[UpOne][LeftOne];
+						
+						//Code for tile to the upper right
+						z[5]= y[UpOne][RightOne];
+						
+						//code for tile to the bottom right
+						z[6]= y[DownOne][RightOne];
+						
+						//code for tile to the bottom left
+						z[7]= y[DownOne][LeftOne];
+						
+							for(int o=0; o<z.length;o++)
+							{
+								System.out.println(z[o]);
+							}
+							System.out.println("/n");
+							
+						Tile hi = x[r][c].getUpdatedTile(z);
+						y[r][c] = hi;
+					}
 				}
 			}
+			
 			x = y;
 			redraw(rows, columns, x);
 			core.API.pause(pauseMs);
