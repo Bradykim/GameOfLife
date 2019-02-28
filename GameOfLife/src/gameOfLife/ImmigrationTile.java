d5208fa528ab03d598ee58b25c42ae12e439038b
s-kimbr <s-kimbr@51S50023421.it.bsd405.org>
2019-02-28 13:21:08 -0800

    ddadwdwaw

---
+++ GameOfLife/src/gameOfLife/ImmigrationTile.java
@@ -0,0 +1,53 @@
+package gameOfLife;
+
+import java.awt.Color;
+
+public class ImmigrationTile extends LifeTile
+{
+	public ImmigrationTile(int initialAge)
+	{
+		super(initialAge);
+	}
+	public Color getColor()
+	{
+		Color r = null;
+		if(getAge()==0)
+		{
+			r= Color.BLACK;
+		}
+		else if(getAge()==1)
+		{
+			r= Color.GREEN;
+		}
+		else if(getAge()==2)
+		{
+			r= Color.BLUE;
+		}
+		return r;
+	}
+	public Tile getUpdatedTile(Tile[] neighbors)
+	{
+		int active= getNumActiveNeighbors(neighbors);
+		if(getAge()>0)
+		{
+			if(active==2||active==3)
+			{
+				return new ImmigrationTile(getAge()+1);
+			}
+			else
+			{
+				return new ImmigrationTile(0);
+			}
+		}
+		else
+		{
+			if(active==3)
+			{
+				return new ImmigrationTile(1);
+			}
+			
+		}
+		return new ImmigrationTile(0);
+	}
+	
+}
