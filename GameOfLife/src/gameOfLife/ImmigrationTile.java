8eb623ff1791bb42d7c1f219bcd7840484ab4a1f
s-kimbr <s-kimbr@51S50023421.it.bsd405.org>
2019-03-01 12:56:57 -0800

    xxdwadwadawdwa

--- GameOfLife/src/gameOfLife/ImmigrationTile.java
+++ GameOfLife/src/gameOfLife/ImmigrationTile.java
@@ -12,11 +12,11 @@ public class ImmigrationTile extends LifeTile
 		{
 			currentcolor= Color.BLACK;
 		}
-		else if(getAge()==1)
+		else if(initialAge==1)
 		{
 			currentcolor= Color.GREEN;
 		}
-		else if(getAge()==2)
+		else if(initialAge==2)
 		{
 			currentcolor= Color.BLUE;
 		}
@@ -25,6 +25,23 @@ public class ImmigrationTile extends LifeTile
 	{
 		super(initialAge);
 		currentcolor=c;
+	}
+	public Color getColor()
+	{
+		Color c=null;
+		if(getAge()==0)
+		{
+			c= Color.BLACK;
+		}
+		else if(getAge()==1)
+		{
+			c= Color.GREEN;
+		}
+		else if(getAge()==2)
+		{
+			c= Color.BLUE;
+		}
+		return c;
 	}
 
 
